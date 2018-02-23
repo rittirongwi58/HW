9152f461f31fb722c19aef8e3636048f4bd92b13
rittirongwi58 <36768102+rittirongwi58@users.noreply.github.com>
2018-02-23 22:42:51 +0700

    Add files via upload

---
+++ db.php.php
@@ -0,0 +1,18 @@
+﻿<?php
+class Database{
+    private $host ="localhost";
+    private $dbname ="courses";
+    private $username ="root";
+    public $conn;
+    public function Connectiondb(){
+        try{
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->username);
+            
+            $this->conn->exec("set names utf8");
+}catch(PDOException $e){
+    echo "Connection error:".$e->getMessage();
+}
+    return $this->conn;
+    }
+}
+?> 
---
+++ exec.php
@@ -0,0 +1,49 @@
+﻿<?php
+class ExecSQL{
+    private $conn;
+     public function __construct($str_conn){
+        $this->conn =$str_conn;
+     }    
+    public function readAll($tablename){
+         $stmt =$this->conn->prepare("SELECT * FROM ".$tablename);
+         $stmt ->execute();
+         return $stmt;
+    }    
+    public function rowCount($tablename){
+        $stmt = $this->conn->prepare("SELECT  COUNT(*) AS total_row FROM ".$tablename);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['total_row'];  
+     }
+    public function readOne($command,$tablename,$cond){
+        $stmt = $this->conn->prepare(" ".$command."  ".$tablename." ".$cond." ");
+        if($command == "DELETE FROM")
+        {
+            return $this->EXEC($stmt);
+        }
+        if($command == "SELECT * FROM")
+        {
+            $stmt->execute();
+            return $stmt; 
+        } 
+     }
+    public function update($tablename,$filde,$cond)
+     {
+        $stmt = $this->conn->prepare(" UPDATE ".$tablename." SET ".$filde." ".$cond."");
+        return $this->EXEC($stmt);
+     }
+//add ข้อมูล
+    public function insert($tablename,$Field,$Value)
+     {
+         $stmt = $this->conn->prepare("INSERT INTO ".$tablename." (".$Field.") VALUES (".$Value.") ");
+         return $this->EXEC($stmt);
+         
+     }
+    public function EXEC($stmt)
+     {
+        if($stmt->execute()) return true;
+        else return false;
+     }
+     
+}
+?>
